441b5eae272463c83b37f7a8f3c0cdcc1a0e744e
Amir Nazari <nazarami2000@gmail.com>
2022-02-06 21:01:40 +1100

    Added a percentage to the progress

--- components/Body/BeforeUpload/main.tsx
+++ components/Body/BeforeUpload/main.tsx
@@ -1,6 +1,8 @@
 import Body from "./Body/Main";
 import Top from "./TopBar";
 import BottomBar from "./BottomBar";
+// import styles from "styles/body/main.module.scss";
+
 function main() {
   return (
     <div>
