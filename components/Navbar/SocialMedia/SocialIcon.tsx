edb08aef058a36514b2f257d81b07ebc3d535350
Amir Nazari <nazarami2000@gmail.com>
2022-02-08 19:25:37 +1100

    Changed file names for deployment

--- components/Navbar/SocialMedia/SocialIcon.tsx
+++ components/Navbar/SocialMedia/SocialIcon.tsx
@@ -2,7 +2,7 @@ import { ContactInfo } from "types";
 function SocialIcon({ Icon, link }: ContactInfo) {
   return (
     <div>
-      <a target="_blank" href={link}>
+      <a target="_blank" href={link} rel="noreferrer">
         <Icon />
       </a>
     </div>
