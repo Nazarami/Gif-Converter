fe6f934885808817884b506a6b7973ac0a2a1a08
Amir Nazari <nazarami2000@gmail.com>
2022-02-08 16:00:11 +1100

    Finished drag and drop API

--- components/Body/BeforeUpload/Body/Main.tsx
+++ components/Body/BeforeUpload/Body/Main.tsx
@@ -2,9 +2,20 @@ import styles from "styles/body/beforeUpload/body.module.scss";
 import UploadIcon from "public/body/upload.svg";
 import FileUpload from "./FileUpload";
 
+import { useDropzone } from "react-dropzone";
+import { useContext } from "react";
+import { videoContext } from "Context";
+
 function beforeUpload() {
+  const { setVideo } = useContext(videoContext);
+  const { getRootProps } = useDropzone({
+    accept: "video/*",
+    onDrop: (acceptedFiles) => {
+      setVideo(acceptedFiles[0]);
+    },
+  });
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper} {...getRootProps()}>
       <div className={styles.dottedOutline}>
         <UploadIcon height="35px" width="35px" />
         <h1 className={styles.videoWrapperHeader}>Drag and drop a video</h1>
